03c2298520a62b8764f8606d7bcca2170ae4cd28
AkshataABhat <akshatavidisha@gmail.com>
2023-06-13 22:57:53 +0530

    Update part5

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/AkshataABhat/Resume/main/Akshata_Bhat_Resume.pdf";
+  "https://raw.githubusercontent.com/AkshataABhat/Resume/main/Akshata_A_Bhat_20BCE0923_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -40,11 +40,7 @@ function ResumeNew() {
              
 
           </Document>
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-             
 
-          </Document>
         </Row>
 
         
